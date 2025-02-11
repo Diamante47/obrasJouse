c90d5c4674e3663a357b267864fe6e7836132ca1
Diamante47 <137074345+Diamante47@users.noreply.github.com>
2025-02-10 23:31:46 -0500

    Update settings.py

--- ObrasPublicas/settings.py
+++ ObrasPublicas/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'Aplicaciones.obras'
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ObrasPublicas.urls'
